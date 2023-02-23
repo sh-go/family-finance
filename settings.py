76ddf5a3a23a7eb802bd90149c631e739064ea77
sh-go <alive.vb.s2@gmail.com>
2023-02-23 12:53:32 +0900

    requirements.txtを設置する #1

--- settings.py
+++ settings.py
@@ -8,15 +8,15 @@ load_dotenv(dotenv_path)
 DISCODE_BOT_TOKEN_FAMILYFINANCE = os.environ.get("DISCODE_BOT_TOKEN_FAMILYFINANCE")
 
 CREDENTIALS = {
-    "type": os.environ.get("TYPE"),
+    "type": "service_account",
     "project_id": os.environ.get("PROJECT_ID"),
     "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
     "private_key": os.environ.get("PRIVATE_KEY"),
     "client_email": os.environ.get("CLIENT_EMAIL"),
     "client_id": os.environ.get("CLIENT_ID"),
-    "auth_uri": os.environ.get("AUTH_URI"),
-    "token_uri": os.environ.get("TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL"),
 }
 
