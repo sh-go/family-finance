e9bbbd12e2b41a03ff81cb514b6a87554664e2bd
sh-go <alive.vb.s2@gmail.com>
2024-09-11 21:30:23 +0900

    スクリーンショットを取得してエラー検証

--- dl-ul-csv.py
+++ dl-ul-csv.py
@@ -118,7 +118,7 @@ elem_dlcsv.click()
 browser.save_screenshot("s3.png")
 
 # 一時保存フォルダ内に".crdownload"の拡張子ファイルがある場合は待機
-browser.get("https://moneyforward.com/cf/csv?from=2024%2F09%2F01&month=9&year=2024")
+browser.get("https://moneyforward.com/cf/csv?from=2024%2F08%2F01&month=8&year=2024")
 timeout_sec = 10
 success_flg = True
 while glob.glob("/*.crdownload") != []:
